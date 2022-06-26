fc7fb5d787d9a1b91b5c98004be7fb9f7ba24830
irfanmuhammadkhan <aiwithimk@gmail.com>
2022-06-27 03:51:15 +0500

    Saylani ID Card assignment

---
+++ saylani_id_card.py
@@ -0,0 +1,23 @@
+name = input("Enter your name: ")
+father_name = input("Enter your father's name: ")
+cnic = input("Enter your CNIC Number: ")
+roll_number = input("Enter your Roll Number: ")
+batch = input("Enter your Batch number: ")
+
+
+saylani_id_card = f"""
+***********************************************
+
+     Saylani Mass IT Training Program
+     
+Artificial Intelligence & Data Science Course
+
+Name          : {name}
+Father's Name : {father_name}
+CNIC Name     : {cnic}
+Roll Number   : {roll_number}
+Batch Number  : {batch}
+
+***********************************************
+"""
+print(saylani_id_card)
